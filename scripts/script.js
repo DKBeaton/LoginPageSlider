1bbe01f53690459658ab149063eb8dbf69f1ebcb
Daniel Beaton <D.Beaton@hotmail.com>
2020-09-01 16:34:40 -0230

    Added input animation

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,47 @@
+// Button Elements
 const signInBtn = document.getElementById('sign-in-btn');
 const signUpBtn = document.getElementById('sign-up-btn');
-const containerElement = document.getElementById('container');
-
 const signInSpan = document.getElementById('sign-in-span');
 const signUpSpan = document.getElementById('sign-up-span');
 
+// Other Elements
+const containerElement = document.getElementById('container');
+const inputElements = document.querySelectorAll('.input');
+
+// inputElements[0].addEventListener('focus', (event) => {
+//   event.target.style.background = 'pink';    
+// });
+
+// inputElements[0].addEventListener('blur', (event) => {
+//   event.target.style.background = '';    
+// });
+
+// Focus Function
+function focusFunction () {
+
+  // Get input container element
+  let parentElement = this.parentNode;
+
+  // Add focus class to input element
+  parentElement.classList.add('focus');
+}
+
+// Unfocus Function
+function unfocusFunction () {
+
+  // Get input container element
+  let parentElement = this.parentNode;
+
+  // Check if input has text
+  if (this.value == "") {
+    parentElement.classList.remove('focus');
+  }
+}
+
+inputElements.forEach(input => {
+  input.addEventListener('focus', focusFunction);
+  input.addEventListener('blur', unfocusFunction);
+});
 
 signInBtn.addEventListener('click', function () {
   containerElement.classList.remove('sign-up-active');
